f96c2e90eae1263400041adb80a8878fac14a900
HUJameson <185543017@qq.com>
2025-03-11 14:56:32 +0800

    update docker commands

--- document/sh/mall-admin.sh
+++ document/sh/mall-admin.sh
@@ -2,6 +2,7 @@
 app_name='mall-admin'
 docker login --username=hujameson@gmail.com --password=Tender512 registry.cn-hangzhou.aliyuncs.com
 docker pull registry.cn-hangzhou.aliyuncs.com/cr-mall/${app_name}:1.0-SNAPSHOT
+docker tag registry.cn-hangzhou.aliyuncs.com/cr-mipa/${app_name}:1.0-SNAPSHOT ${app_name}:1.0-SNAPSHOT
 CID_SERVICE=$(docker ps | grep "${app_name}" | awk '{print $1}')
 if [ -n "$CID_SERVICE" ]; then
         echo "存在${app_name}容器，CID_SERVICE =$CID_SERVICE，停止docker容器 ..."

67eb9b173971dd707c27b1dad90b1355e7cdc80e
HUJameson <185543017@qq.com>
2025-03-14 14:48:15 +0800

    initial commit

--- document/sh/mall-admin.sh
+++ document/sh/mall-admin.sh
@@ -1,19 +1,22 @@
-#!/usr/bin/env bash
-app_name='mall-admin'
-docker login --username=hujameson@gmail.com --password=Tender512 registry.cn-hangzhou.aliyuncs.com
-docker pull registry.cn-hangzhou.aliyuncs.com/cr-mall/${app_name}:1.0-SNAPSHOT
-docker tag registry.cn-hangzhou.aliyuncs.com/cr-mall/${app_name}:1.0-SNAPSHOT ${app_name}:1.0-SNAPSHOT
-CID_SERVICE=$(docker ps | grep "${app_name}" | awk '{print $1}')
+SERVICE_NAME=mall-admin
+#docker build --platform linux/amd64 -t registry.cn-hangzhou.aliyuncs.com/cr-mall/$SERVICE_NAME:1.0-SNAPSHOT
+#docker login --username=hujameson@gmail.com --password=Tender512 registry.cn-hangzhou.aliyuncs.com
+#docker pull registry.cn-hangzhou.aliyuncs.com/cr-mall/$SERVICE_NAME:1.0-SNAPSHOT
+#docker tag registry.cn-hangzhou.aliyuncs.com/cr-mall/$SERVICE_NAME:1.0-SNAPSHOT $SERVICE_NAME:1.0-SNAPSHOT
+docker build --platform linux/amd64 -t $SERVICE_NAME:1.0-SNAPSHOT
+CID_SERVICE=$(docker ps -a | grep "$SERVICE_NAME" | awk '{print $1}')
 if [ -n "$CID_SERVICE" ]; then
-        echo "存在${app_name}容器，CID_SERVICE =$CID_SERVICE，停止docker容器 ..."
-                docker stop -f ${app_name}
-                docker rm -f ${app_name}
-        echo "docker rm -f ${app_name}容器停止完成"
+        echo "存在$SERVICE_NAME容器，CID_SERVICE =$CID_SERVICE，停止docker容器 ..."
+                docker stop -f $SERVICE_NAME
+                docker rm -f $SERVICE_NAME
+        echo "docker rm -f $SERVICE_NAME容器停止完成"
 fi
 echo "docker run创建容器..."
-docker run -p 18080:8080 --name ${app_name} \
+docker run -p 8080:8080 --name $SERVICE_NAME \
+--link redis:redis \
+--link logstash:logstash \
 -e TZ="Asia/Shanghai" \
 -v /etc/localtime:/etc/localtime \
--v /Users/huchangzheng/mydata/app/${app_name}/logs:/var/logs \
--d ${app_name}:1.0-SNAPSHOT
-echo "${app_name}容器创建完成"
+-v /mydata/app/admin/logs:/var/logs \
+-d $SERVICE_NAME:1.0-SNAPSHOT
+echo "$SERVICE_NAME容器创建完成"

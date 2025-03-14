a0c61ba9a2020650d3914c4b9eda3101673ab1c8
HUJameson <185543017@qq.com>
2025-03-14 15:46:28 +0800

    minor change

---
+++ mall-ci.sh
@@ -0,0 +1,6 @@
+mkdir upload || true
+cp mall-admin/target/mall-admin-1.0-SNAPSHOT.jar upload/mall-admin-1.0-SNAPSHOT.jar
+#cp mall-search/target/mall-search-1.0-SNAPSHOT.jar upload/mall-search-1.0-SNAPSHOT.jar
+#cp mall-portal/target/mall-portal-1.0-SNAPSHOT.jar upload/mall-portal-1.0-SNAPSHOT.jar
+cp document/sh/Dockerfile-mall-admin upload/Dockerfile-mall-admin
+cp document/sh/mall-admin.sh upload/mall-admin.sh

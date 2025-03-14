27292a95f7aa2e27f38141ec6fb192e1ec7b72bb
HUJameson <185543017@qq.com>
2025-03-14 15:20:41 +0800

    fix bug

--- document/sh/mall-admin.sh
+++ document/sh/mall-admin.sh
@@ -1,9 +1,11 @@
 SERVICE_NAME=mall-admin
+DOCFILE_NAME=Dockerfile-mall-admin
 #docker build --platform linux/amd64 -t registry.cn-hangzhou.aliyuncs.com/cr-mall/$SERVICE_NAME:1.0-SNAPSHOT
 #docker login --username=hujameson@gmail.com --password=Tender512 registry.cn-hangzhou.aliyuncs.com
 #docker pull registry.cn-hangzhou.aliyuncs.com/cr-mall/$SERVICE_NAME:1.0-SNAPSHOT
 #docker tag registry.cn-hangzhou.aliyuncs.com/cr-mall/$SERVICE_NAME:1.0-SNAPSHOT $SERVICE_NAME:1.0-SNAPSHOT
-docker build --platform linux/amd64 -t $SERVICE_NAME:1.0-SNAPSHOT
+mv $DOCFILE_NAME Dockerfile
+docker build --platform linux/amd64 -t $SERVICE_NAME:1.0-SNAPSHOT .
 CID_SERVICE=$(docker ps -a | grep "$SERVICE_NAME" | awk '{print $1}')
 if [ -n "$CID_SERVICE" ]; then
         echo "存在$SERVICE_NAME容器，CID_SERVICE =$CID_SERVICE，停止docker容器 ..."

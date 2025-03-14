5f0ca3490e8a967e61788fe02954ebf315d02803
HUJameson <185543017@qq.com>
2025-03-14 11:32:58 +0800

    remove the steps for build docker images

---
+++ document/sh/mall-admin-cd.sh
@@ -0,0 +1,19 @@
+SERVICE_NAME=mall-admin
+docker login --username=hujameson@gmail.com --password=Tender512 registry.cn-hangzhou.aliyuncs.com
+docker pull registry.cn-hangzhou.aliyuncs.com/cr-mall/$SERVICE_NAME:1.0-SNAPSHOT
+docker tag registry.cn-hangzhou.aliyuncs.com/cr-mall/$SERVICE_NAME:1.0-SNAPSHOT $SERVICE_NAME:1.0-SNAPSHOT
+CID_SERVICE=$(docker ps -a | grep "$SERVICE_NAME" | awk '{print $1}')
+if [ -n "$CID_SERVICE" ]; then
+        echo "存在$SERVICE_NAME容器，CID_SERVICE =$CID_SERVICE，停止docker容器 ..."
+                docker stop -f $SERVICE_NAME
+                docker rm -f $SERVICE_NAME
+        echo "docker rm -f $SERVICE_NAME容器停止完成"
+fi
+echo "docker run创建容器..."
+docker run -p 8080:8080 --name $SERVICE_NAME \
+--link redis:redis \
+-e TZ="Asia/Shanghai" \
+-v /etc/localtime:/etc/localtime \
+-v /mydata/app/admin/logs:/var/logs \
+-d $SERVICE_NAME:1.0-SNAPSHOT
+echo "$SERVICE_NAME容器创建完成"
